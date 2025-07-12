fa1870b79121a620f7f7682f566c84a0c1a6febf
vicky <vigneshhari702@gmail.com>
2025-07-12 22:10:03 +0530

    saved app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -35,4 +36,5 @@ def index():
     return render_template('index.html', weather=weather, error=error)
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT env variable or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
